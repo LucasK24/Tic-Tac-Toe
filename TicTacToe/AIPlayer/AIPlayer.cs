cce46081e1df8f0ecf3be1cf73e645c47e93d5b7
LucasK24 <38139322+LucasK24@users.noreply.github.com>
2021-07-23 16:26:50 -0600

    Made changes to AIPlayer's MakeHardMove() to now have lists of winning and losing moves. This imporves randomness... Need to finish implementing the CompleteRow helper and fix minor bugs.

--- TicTacToe/AIPlayer/AIPlayer.cs
+++ TicTacToe/AIPlayer/AIPlayer.cs
@@ -5,6 +5,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace CPUPlayer
@@ -16,7 +17,12 @@ namespace CPUPlayer
     {
         // Represents difficulty level ("easy", "hard", or "impossible")
         private string difficulty;
+        
+        // Random generator for picking moves.
         private Random rand;
+
+        // Dictionary mapping rowID to tuples of board row/column/diagonal.
+        private Dictionary<int, Tuple<Tuple<int,int>, Tuple<int, int>, Tuple<int, int>>> allRows;
 
         /// <summary>
         /// 
@@ -25,6 +31,16 @@ namespace CPUPlayer
         {
             difficulty = difficultyLevel;
             rand = new Random();
+
+            // Add the 8 different "rows" to win.
+            allRows[0] = new Tuple<int, int>;
+            allRows[1] = ;
+            allRows[2] = ;
+            allRows[3] = ;
+            allRows[4] = ;
+            allRows[5] = ;
+            allRows[6] = ;
+            allRows[7] = ;
         }
 
         /// <summary>
@@ -70,87 +86,127 @@ namespace CPUPlayer
         /// <returns>A tuple of the column-row pair</returns>
         private Tuple<int, int> MakeHardMove(char[,] board)
         {
-            // If winning move....
-            // Check all 8 rows for ways to win now.
-            PossibleWinCheck(board);
-            if ((board[0, 0] == 'O' && board[0, 1] == 'O' && board[0, 2] == '\u0000')
-                || (board[1, 0] == 'O' && board[1, 1] == 'O' && board[1, 2] == 'O')
-                || (board[2, 0] == 'O' && board[2, 1] == 'O' && board[2, 2] == 'O')
-                || (board[0, 0] == 'O' && board[1, 0] == 'O' && board[2, 0] == 'O')
-                || (board[0, 1] == 'O' && board[1, 1] == 'O' && board[2, 1] == 'O')
-                || (board[0, 2] == 'O' && board[1, 2] == 'O' && board[2, 2] == 'O')
-                || (board[0, 0] == 'O' && board[1, 1] == 'O' && board[2, 2] == 'O')
-                || (board[0, 2] == 'O' && board[1, 1] == 'O' && board[2, 0] == 'O'))
-            {
-                return new Tupple<int, int>(0, 0);
-            }
+            // Check all 8 rows/cols/diagonals for ways to win now, returning the winning move if there is one.
+            Tuple<int, int> winningMove = PossibleWinCheck(board, 'O');
+            if (winningMove != null)
+                return winningMove;
 
-            // If losing move to block....
+            // Check if there is a way to lose when the human player goes next, Making that move if there is one.
+            Tuple<int, int> losingMove = PossibleWinCheck(board, 'X');
+            if (losingMove != null)
+                return losingMove;
 
+
+            // Otherwise, make a random move.
             return MakeEasyMove(board);
         }
 
         /// <summary>
-        /// 
+        /// Checks if the input letter (X or O) has any possible moves to win. For the AI, 
+        /// passing in an 'O' will check for the AI's winning moves, while passing
+        /// in an 'X' will check for winning moves of the human player that the AI
+        /// can block.
         /// </summary>
         /// <param name="board"></param>
-        /// <returns></returns>
-        private Tuple<int, int> PossibleWinCheck(char[,] board)
+        /// <returns>A Tuple<int,int> of the winning move, null if there isn't one.</returns>
+        private Tuple<int, int> PossibleWinCheck(char[,] board, char letter)
         {
+            List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>> WinningMoves = new List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>>();
+            List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>> LosingMoves = new List<Tuple<Tuple<int, int>, Tuple< int, int>, Tuple<int,int>>> ();
+            // Check each possible row/col/diagonal for a win/loss. If there is one, add to winning or losing move list.
+            CheckRow(board[0, 0], board[0, 1], board[0, 2], WinningMoves, LosingMoves);
 
-            int OCounter = 0;
-            int XCounter = 0;
 
-            CheckRow(board[0, 0], board[0, 1], board[0, 2]);
-            // Do CheckRow for the rest.....
-            if ((board[0, 0] == 'O' && board[0, 1] == 'O' && board[0, 2] == '\u0000')
+            // CheckRow could now be: CheckRow(*rowID*, board);
 
-                || (board[1, 0] == 'O' && board[1, 1] == 'O' && board[1, 2] == 'O')
-                || (board[2, 0] == 'O' && board[2, 1] == 'O' && board[2, 2] == 'O')
-                || (board[0, 0] == 'O' && board[1, 0] == 'O' && board[2, 0] == 'O')
-                || (board[0, 1] == 'O' && board[1, 1] == 'O' && board[2, 1] == 'O')
-                || (board[0, 2] == 'O' && board[1, 2] == 'O' && board[2, 2] == 'O')
-                || (board[0, 0] == 'O' && board[1, 1] == 'O' && board[2, 2] == 'O')
-                || (board[0, 2] == 'O' && board[1, 1] == 'O' && board[2, 0] == 'O'))
+            //.....
+            return CompleteRow(board[0, 0], board[0, 1], board[0, 2]);
+            if (CheckRow(board[2, 0], board[2, 1], board[2, 2]))
+                return CompleteRow(board[2, 0], board[2, 1], board[2, 2]);
+            if (CheckRow(board[0, 0], board[1, 0], board[2, 0]))
+                return CompleteRow(board[0, 0], board[1, 0], board[2, 0]);
+            if (CheckRow(board[0, 1], board[1, 1], board[2, 1]))
+                return CompleteRow(board[0, 1], board[1, 1], board[2, 1]);
+            if (CheckRow(board[0, 1], board[1, 1], board[2, 1]))
+                return CompleteRow(board[0, 1], board[1, 1], board[2, 1]);
+            if (CheckRow(board[0, 2], board[1, 2], board[2, 2]))
+                return CompleteRow(board[0, 2], board[1, 2], board[2, 2]);
+            if (CheckRow(board[0, 2], board[1, 1], board[2, 0]))
+                return CompleteRow(board[0, 2], board[1, 1], board[2, 0]);
+            if (CheckRow(board[0, 0], board[1, 1], board[2, 2]))
+                return CompleteRow(board[0, 0], board[1, 1], board[2, 2]);
+
+
+            // Get a move from the winning list, if any.
+            if (WinningMoves.Count != 0)
             {
-                return new Tuple<int, int>(0, 0);
+                int move = rand.Next(WinningMoves.Count);
+                return CompleteRow(....);
             }
+
+            // If there are no winning moves, block a loss from happening if possible.
+            if (LosingMoves.Count != 0)
             {
-                return new Tuple<int, int>(0, 0);
+                int move = rand.Next(LosingMoves.Count);
+                return CompleteRow(....);
             }
-            return null;
+
+            // Otherwise return a random move.
+            return MakeEasyMove(board);
         }
 
         /// <summary>
-        /// Fill in!!!!
+        /// Uses counters for 'X's and 'O's to see if this row/column/diagonal has a winning move for either player.
+        /// Adds to the correct list the rowID if so.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="c"></param>
-        /// <returns></returns>
-        private bool CheckRow(char first, char second, char third)
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <param name="rowID"></param>
+        /// <param name="WinningMoves"></param>
+        /// <param name="LosingMoves"></param>
+        private void CheckRow(char first, char second, char third, int rowID, List<int> WinningMoves, List<int> LosingMoves)
         {
             int OCounter = 0;
             int XCounter = 0;
-            if (a == 'O')
+            if (first == 'O')
                 OCounter++;
-            else if (a == 'X')
+            else if (first == 'X')
                 XCounter++;
 
-            if (b == 'O')
+            if (second == 'O')
                 OCounter++;
-            else if (b == 'X')
+            else if (second == 'X')
                 XCounter++;
 
-            if (c == 'O')
+            if (third == 'O')
                 OCounter++;
-            else if (c == 'X')
+            else if (third == 'X')
                 XCounter++;
 
-            // Find the empty space if O Counter is 2 and X counter is 0.
+            // Instead: return number corresponding to the correct row.
             if (OCounter == 2 && XCounter == 0)
-                return 
+                WinningMoves.Add(rowID);
 
+            if (XCounter == 2 && OCounter == 0)
+                LosingMoves.Add(rowID);
+        }
+
+        /// <summary>
+        /// Returns the open move in this row, if there is one. Returns null otherwise.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns></returns>
+        private Tuple<int, int> CompleteRow(char first, char second, char third)
+        {
+            if (first == '\u0000')
+                return first;
+            else if
+            else if
+            else
+                                return null;
         }
 
 
