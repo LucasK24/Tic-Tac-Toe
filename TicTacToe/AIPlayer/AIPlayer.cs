9b0325be7e5170aa336d3a313afc863b29e8971c
LucasK24 <38139322+LucasK24@users.noreply.github.com>
2021-07-24 00:27:22 -0600

    Made adjustments to AIPlayer methods to be more readable and efficient. Need to complete CheckRow and CompleteRow based on these changes.

--- TicTacToe/AIPlayer/AIPlayer.cs
+++ TicTacToe/AIPlayer/AIPlayer.cs
@@ -22,25 +22,56 @@ namespace CPUPlayer
         private Random rand;
 
         // Dictionary mapping rowID to tuples of board row/column/diagonal.
-        private Dictionary<int, Tuple<Tuple<int,int>, Tuple<int, int>, Tuple<int, int>>> allRows;
+        private Dictionary<int, List<Tuple<int,int>>> allRows;
 
         /// <summary>
-        /// 
+        /// Creates an AI player with the input difficulty level. May be "Easy", "Hard", or "Impossible.
         /// </summary>
         public AIPlayer(string difficultyLevel)
         {
             difficulty = difficultyLevel;
             rand = new Random();
 
-            // Add the 8 different "rows" to win.
-            allRows[0] = new Tuple<int, int>;
-            allRows[1] = ;
-            allRows[2] = ;
-            allRows[3] = ;
-            allRows[4] = ;
-            allRows[5] = ;
-            allRows[6] = ;
-            allRows[7] = ;
+            // Add the 8 different "rows" to allRows dictionary.
+            allRows[0] = new List<Tuple<int,int>>();
+            allRows[0].Add(new Tuple<int,int>(0,0));
+            allRows[0].Add(new Tuple<int, int>(0, 1));
+            allRows[0].Add(new Tuple<int, int>(0, 2));
+
+            allRows[1] = new List<Tuple<int, int>>();
+            allRows[1].Add(new Tuple<int, int>(1, 0));
+            allRows[1].Add(new Tuple<int, int>(1, 1));
+            allRows[1].Add(new Tuple<int, int>(1, 2));
+
+            allRows[2] = new List<Tuple<int, int>>();
+            allRows[2].Add(new Tuple<int, int>(2, 0));
+            allRows[2].Add(new Tuple<int, int>(2, 1));
+            allRows[2].Add(new Tuple<int, int>(2, 2));
+
+            allRows[3] = new List<Tuple<int, int>>();
+            allRows[3].Add(new Tuple<int, int>(0, 0));
+            allRows[3].Add(new Tuple<int, int>(1, 0));
+            allRows[3].Add(new Tuple<int, int>(2, 0));
+            
+            allRows[4] = new List<Tuple<int, int>>();
+            allRows[4].Add(new Tuple<int, int>(0, 1));
+            allRows[4].Add(new Tuple<int, int>(1, 1));
+            allRows[4].Add(new Tuple<int, int>(2, 1));
+
+            allRows[5] = new List<Tuple<int, int>>();
+            allRows[5].Add(new Tuple<int, int>(0, 2));
+            allRows[5].Add(new Tuple<int, int>(1, 2));
+            allRows[5].Add(new Tuple<int, int>(2, 2));
+            
+            allRows[6] = new List<Tuple<int, int>>();
+            allRows[6].Add(new Tuple<int, int>(0, 2));
+            allRows[6].Add(new Tuple<int, int>(1, 1));
+            allRows[6].Add(new Tuple<int, int>(2, 0));
+
+            allRows[7] = new List<Tuple<int, int>>();
+            allRows[7].Add(new Tuple<int, int>(0, 0));
+            allRows[7].Add(new Tuple<int, int>(1, 1));
+            allRows[7].Add(new Tuple<int, int>(2, 2));
         }
 
         /// <summary>
@@ -79,76 +110,39 @@ namespace CPUPlayer
         }
 
         /// <summary>
-        /// Will make a winning move if there is one and will block losses if possible.
+        /// Will make a winning move if there is one and will block losses from occuring if possible.
         /// Otherwise, makes a random move.
         /// </summary>
         /// <param name="board"></param>
-        /// <returns>A tuple of the column-row pair</returns>
+        /// <returns>A tuple of the AI's move (a column-row pair)</returns>
         private Tuple<int, int> MakeHardMove(char[,] board)
         {
-            // Check all 8 rows/cols/diagonals for ways to win now, returning the winning move if there is one.
-            Tuple<int, int> winningMove = PossibleWinCheck(board, 'O');
-            if (winningMove != null)
-                return winningMove;
+            // Lists that keep track of the RowIDs of winning moves to make or losing moves to block.
+            List<int> WinningMoves = new List<int>();
+            List<int> LosingMoves = new List<int>();
 
-            // Check if there is a way to lose when the human player goes next, Making that move if there is one.
-            Tuple<int, int> losingMove = PossibleWinCheck(board, 'X');
-            if (losingMove != null)
-                return losingMove;
-
-
-            // Otherwise, make a random move.
-            return MakeEasyMove(board);
-        }
-
-        /// <summary>
-        /// Checks if the input letter (X or O) has any possible moves to win. For the AI, 
-        /// passing in an 'O' will check for the AI's winning moves, while passing
-        /// in an 'X' will check for winning moves of the human player that the AI
-        /// can block.
-        /// </summary>
-        /// <param name="board"></param>
-        /// <returns>A Tuple<int,int> of the winning move, null if there isn't one.</returns>
-        private Tuple<int, int> PossibleWinCheck(char[,] board, char letter)
-        {
-            List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>> WinningMoves = new List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>>();
-            List<Tuple<Tuple<int, int>, Tuple<int, int>, Tuple<int, int>>> LosingMoves = new List<Tuple<Tuple<int, int>, Tuple< int, int>, Tuple<int,int>>> ();
             // Check each possible row/col/diagonal for a win/loss. If there is one, add to winning or losing move list.
-            CheckRow(board[0, 0], board[0, 1], board[0, 2], WinningMoves, LosingMoves);
-
-
-            // CheckRow could now be: CheckRow(*rowID*, board);
-
-            //.....
-            return CompleteRow(board[0, 0], board[0, 1], board[0, 2]);
-            if (CheckRow(board[2, 0], board[2, 1], board[2, 2]))
-                return CompleteRow(board[2, 0], board[2, 1], board[2, 2]);
-            if (CheckRow(board[0, 0], board[1, 0], board[2, 0]))
-                return CompleteRow(board[0, 0], board[1, 0], board[2, 0]);
-            if (CheckRow(board[0, 1], board[1, 1], board[2, 1]))
-                return CompleteRow(board[0, 1], board[1, 1], board[2, 1]);
-            if (CheckRow(board[0, 1], board[1, 1], board[2, 1]))
-                return CompleteRow(board[0, 1], board[1, 1], board[2, 1]);
-            if (CheckRow(board[0, 2], board[1, 2], board[2, 2]))
-                return CompleteRow(board[0, 2], board[1, 2], board[2, 2]);
-            if (CheckRow(board[0, 2], board[1, 1], board[2, 0]))
-                return CompleteRow(board[0, 2], board[1, 1], board[2, 0]);
-            if (CheckRow(board[0, 0], board[1, 1], board[2, 2]))
-                return CompleteRow(board[0, 0], board[1, 1], board[2, 2]);
-
+            CheckRow(board, 0, WinningMoves, LosingMoves); // RowID: 0
+            CheckRow(board, 1, WinningMoves, LosingMoves); // RowID: 1
+            CheckRow(board, 2, WinningMoves, LosingMoves); // ....           
+            CheckRow(board, 3, WinningMoves, LosingMoves);
+            CheckRow(board, 4, WinningMoves, LosingMoves);
+            CheckRow(board, 5, WinningMoves, LosingMoves);
+            CheckRow(board, 6, WinningMoves, LosingMoves);
+            CheckRow(board, 7, WinningMoves, LosingMoves);
 
             // Get a move from the winning list, if any.
             if (WinningMoves.Count != 0)
             {
                 int move = rand.Next(WinningMoves.Count);
-                return CompleteRow(....);
+                return CompleteRow(board, WinningMoves[move]);
             }
 
             // If there are no winning moves, block a loss from happening if possible.
             if (LosingMoves.Count != 0)
             {
                 int move = rand.Next(LosingMoves.Count);
-                return CompleteRow(....);
+                return CompleteRow(board, LosingMoves[move]);
             }
 
             // Otherwise return a random move.
@@ -165,8 +159,14 @@ namespace CPUPlayer
         /// <param name="rowID"></param>
         /// <param name="WinningMoves"></param>
         /// <param name="LosingMoves"></param>
-        private void CheckRow(char first, char second, char third, int rowID, List<int> WinningMoves, List<int> LosingMoves)
+        private void CheckRow(char[,] board, int rowID, List<int> WinningMoves, List<int> LosingMoves)
         {
+            // Based on the rowID, get the current state of each slot in the row.
+            char first = board[allRows[rowID].Item1.Item1, allRows[rowID].Item1.Item2];
+            char second = board[allRows[rowID].Item2.Item1, allRows[rowID].Item2.Item2];
+            char third = board[allRows[rowID].Item3.Item1, allRows[rowID].Item3.Item2];
+
+            // Count the number of 'X's and 'O's in the row.
             int OCounter = 0;
             int XCounter = 0;
             if (first == 'O')
@@ -184,11 +184,10 @@ namespace CPUPlayer
             else if (third == 'X')
                 XCounter++;
 
-            // Instead: return number corresponding to the correct row.
+            // Add rowID to Winning or LosingMoves list based on if there is a winning move for either player.
             if (OCounter == 2 && XCounter == 0)
                 WinningMoves.Add(rowID);
-
-            if (XCounter == 2 && OCounter == 0)
+            else if (XCounter == 2 && OCounter == 0)
                 LosingMoves.Add(rowID);
         }
 
@@ -199,7 +198,7 @@ namespace CPUPlayer
         /// <param name="second"></param>
         /// <param name="third"></param>
         /// <returns></returns>
-        private Tuple<int, int> CompleteRow(char first, char second, char third)
+        private Tuple<int, int> CompleteRow(char[,] int rowID)
         {
             if (first == '\u0000')
                 return first;
